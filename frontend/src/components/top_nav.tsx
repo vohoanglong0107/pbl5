73194d838b35937aed968383411efb3ebc62b079
mamlong34@gmail.com <mamlong34@gmail.com>
2022-04-20 13:28:58 +0700

    Frontend: - experiment with MUI

--- frontend/src/components/top_nav.tsx
+++ frontend/src/components/top_nav.tsx
@@ -1,13 +1,27 @@
-import logo from "../assets/icon.png"
-import styles from "@/styles/TopNav.module.css";
+import logo from "../assets/icon.png";
+import { Box, ThemeProvider, Typography } from "@mui/material";
 import Image from "next/image";
+import { createTheme } from "@mui/system";
+
+const theme = createTheme({
+  typography: {
+    h3: {
+      fontFamily: ["Palette Mosaic", "cursive"].join(","),
+      fontSize: "25px",
+    },
+  },
+});
 const TopNav = () => {
-    return (
-        <div className={styles.topContainer}>
-            <Image src={logo} width={50} height={50} alt="no" />
-            <h3 className={styles.name}>Exploding kittens</h3>
-        </div>
-    );
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <Box display={"inline-flex"}>
+        <Image src={logo} width={50} height={50} alt="no" />
+        <Typography variant="h3" color={"#FFD365"}>
+          Exploding kittens
+        </Typography>
+      </Box>
+    </ThemeProvider>
+  );
+};
 
 export default TopNav;
