22fb4e1f51d028a303609fc324b909ad6cef3725
mamlong34@gmail.com <mamlong34@gmail.com>
2022-06-04 17:24:10 +0700

    Fix chat bubble cause Handpanel to slide up

--- frontend/src/components/chat/ChatBubble.tsx
+++ frontend/src/components/chat/ChatBubble.tsx
@@ -6,51 +6,58 @@ import Typography from "@mui/material/Typography";
 import Chat from "./Chat";
 import Image from "next/image";
 import MessageImage from "@/assets/msg-icon.png";
-import { styled } from '@mui/system';
+import { styled } from "@mui/system";
 import SettingOpenButton from "../setting/SettingOpenButton";
 
-const MsgBubble = styled('button')({
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    borderRadius: '100%',
-    width: '60px',
-    height: '60px',
-    position: 'fixed',
-    bottom: '10px',
-    left: '10px',
-    '&:hover': {
-        borderColor: 'transparent'
-    }
-  });
+const MsgBubble = styled("button")({
+  backgroundColor: "transparent",
+  borderColor: "transparent",
+  borderRadius: "100%",
+  width: "60px",
+  height: "60px",
+  position: "fixed",
+  bottom: "10px",
+  left: "10px",
+  "&:hover": {
+    borderColor: "transparent",
+  },
+});
 
-const NewMsgs = styled('label')({
-  color: 'red',
-  fontSize: '20px',
-  fontWeight: 'bold',
-  position: 'relative',
-  left: '18px',
-  bottom: '53px',
-
-
-})
-
+const NewMsgs = styled("label")({
+  color: "red",
+  fontSize: "20px",
+  fontWeight: "bold",
+  position: "relative",
+  left: "18px",
+  bottom: "53px",
+});
 
 const ChatBubble = () => {
-    const [ isOpen, setOpen ] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-    function handleClickOpen() {
-      setOpen(true);
-    }
-    
-    const newMessages = "";
-    return (
-      <Box>
-        {!isOpen?(<MsgBubble onClick={handleClickOpen} >
-            <Image src={MessageImage} alt="message-image" width={100} height={100} onClick={() => setOpen(true)} />
-            <NewMsgs>{newMessages}</NewMsgs>
-        </MsgBubble>):(<Chat setOpen={setOpen} />)}
-      </Box>
-    );
-}
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  const newMessages = "";
+  return (
+    <Box position={"absolute"}>
+      {!isOpen ? (
+        <MsgBubble onClick={handleClickOpen}>
+          <Image
+            src={MessageImage}
+            alt="message-image"
+            width={100}
+            height={100}
+            onClick={() => setOpen(true)}
+          />
+          <NewMsgs>{newMessages}</NewMsgs>
+        </MsgBubble>
+      ) : (
+        <Chat setOpen={setOpen} />
+      )}
+    </Box>
+  );
+};
 
 export default ChatBubble;
